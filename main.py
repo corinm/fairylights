f0da1e8d42d5473cafd925039c967256e6e4cdd8
Corin Mulliss <corin.mulliss@gmail.com>
2021-05-26 18:46:54 +0100

    Moved config out, trying brightness gradient

--- main.py
+++ main.py
@@ -1,37 +1,39 @@
 import rpi_ws281x as ws
 
 from FlickeringFairyLights import FlickeringFairyLights
+import config
 
-# LED strip configuration:
-LED_COUNT = 50      # Number of LED pixels.
-LED_PIN = 18      # GPIO pin connected to the pixels (18 uses PWM!)
-LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 255     # Set to 0 for darkest and 255 for brightest
-# True to invert the signal (when using NPN transistor level shift)
-LED_INVERT = False
-LED_CHANNEL = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
-strip = ws.Adafruit_NeoPixel(
-    LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip = ws.Adafruit_NeoPixel(config.LED_COUNT, config.LED_PIN, config.LED_FREQ_HZ,
+                             config.LED_DMA, config.LED_INVERT, config.LED_BRIGHTNESS, config.LED_CHANNEL)
 
 strip.begin()
 
 try:
     print("Starting...")
-    flickering = FlickeringFairyLights()
+    # flickering = FlickeringFairyLights()
 
+    # while True:
+    #     leds = flickering.tick()
+
+    #     for i in range(strip.numPixels()):
+    #         led = leds[i]
+    #         r = round(led.red * 255)
+    #         g = round(led.green * 255)
+    #         b = round(led.blue * 255)
+    #         strip.setPixelColorRGB(i, r, g, b)
+    #         strip.show()
+    counter = 0
     while True:
-        leds = flickering.tick()
-
         for i in range(strip.numPixels()):
-            led = leds[i]
-            r = round(led.red * 255)
-            g = round(led.green * 255)
-            b = round(led.blue * 255)
+            r = counter
+            g = 0
+            b = 0
             strip.setPixelColorRGB(i, r, g, b)
             strip.show()
 
+        if (counter < 255):
+            counter = counter + 1
+
 except KeyboardInterrupt:
-    # Cleanup
     print('Done')
