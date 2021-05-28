43ac5d8d927894409238543c93e51e17083e7c06
Corin Mulliss <corin.mulliss@gmail.com>
2021-05-28 19:05:14 +0100

    Trying shades

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import math
 
 from FlickeringFairyLights import FlickeringFairyLights
 import config
+import ledColours
 
 
 strip = ws.Adafruit_NeoPixel(config.LED_COUNT, config.LED_PIN, config.LED_FREQ_HZ,
@@ -14,18 +15,18 @@ strip.begin()
 
 try:
     print("Starting...")
-    flickering = FlickeringFairyLights()
+    # flickering = FlickeringFairyLights()
 
-    while True:
-        leds = flickering.tick()
+    # while True:
+    #     leds = flickering.tick()
 
-        for i in range(strip.numPixels()):
-            led = leds[i]
-            r = round(led.red * 255)
-            g = round(led.green * 255)
-            b = round(led.blue * 255)
-            strip.setPixelColorRGB(i, r, g, b)
-            strip.show()
+    #     for i in range(strip.numPixels()):
+    #         led = leds[i]
+    #         r = round(led.red * 255)
+    #         g = round(led.green * 255)
+    #         b = round(led.blue * 255)
+    #         strip.setPixelColorRGB(i, r, g, b)
+    #         strip.show()
 
     # while True:
     #     bulbs = rainbow(strip.numPixels())
@@ -33,6 +34,13 @@ try:
     #         strip.setPixelColorRGB(
     #             i, math.floor(bulb.red * 255), math.floor(bulb.green * 255), math.floor(bulb.blue * 255))
     #         strip.show()
+
+    while True:
+        for i in range(strip.numPixels()):
+            c = ledColours.flickerColours[i & 11]
+            strip.setPixelColorRGB(i, math.floor(
+                c.red * 255), math.floor(c.green * 255), math.floor(c.blue * 255))
+            strip.show()
 
     # counter = 6
     # while True:
@@ -44,12 +52,12 @@ try:
     #         strip.setPixelColorRGB(i, r ** 2, g ** 2, b ** 2)
     #         strip.show()
 
-            # if ((counter + 1) ** 2 <= 255):
-            #     counter = counter + 1
-            # else:
-            #     break
+       # if ((counter + 1) ** 2 <= 255):
+       #     counter = counter + 1
+       # else:
+       #     break
 
-            # sleep(0.05)
+       # sleep(0.05)
 
 except KeyboardInterrupt:
     print('Done')
