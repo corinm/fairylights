551e0343d8d6ec8e2d86679618ca829499d4c496
Corin Mulliss <corinm@users.noreply.github.com>
2021-05-28 19:27:31 +0100

    Trying flickering with new colours

--- ledColours.py
+++ ledColours.py
@@ -8,7 +8,19 @@ off = Color(rgb=(0, 0, 0))
 
 # flicker3V = Color(rgb=(60/255, 46/255, 9/255))
 # flicker2_4V = Color(rgb=(60*0.25/255, 46*0.25/255, 9*0.25/255))
-flicker3V = Color(rgb=(60/255, 46/255, 9/255))  # Dimmer
-flicker2_4V = angleToColour(73)  # Brighter
+flicker3V = Color(rgb=(99/255, 134/255, 5/255))  # Dimmer
+flicker2_4V = Color(rgb=(100/255, 122/255, 6/255))  # Brighter
 
 flickerColours = list(flicker2_4V.range_to(flicker3V, 11))
+
+# Shades of yellow/orange to white
+# 0 (85, 170, 0)
+# 1 (92, 157, 2)
+# 2 (96, 145, 3)
+# 3 (99, 134, 5)
+# 4 (100, 122, 6)
+# 5 (99, 111, 7)
+# 6 (97, 100, 8)
+# 7 (90, 86, 9)
+# 8 (79, 71, 9)
+# 9 (69, 57, 9)
