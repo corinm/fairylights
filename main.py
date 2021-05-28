bf23c137ce1f6cfe03e495b4f535184603e21acb
Corin Mulliss <corinm@users.noreply.github.com>
2021-05-28 19:54:25 +0100

    Logging

--- main.py
+++ main.py
@@ -37,6 +37,8 @@ try:
 
     while True:
         for s in range(len(ledColours.shades)):
+            print((math.floor(
+                c.red * 255), math.floor(c.green * 255), math.floor(c.blue * 255)))
             for i in range(50):
                 c = ledColours.shades[s]
                 strip.setPixelColorRGB(i, math.floor(
