788e5a0673cadaed431bc48dfb83f7a194616f2a
Corin Mulliss <corin.mulliss@gmail.com>
2021-05-29 00:20:11 +0100

    Implemented dimming for flicker bulbs

--- FlickerBulb.py
+++ FlickerBulb.py
@@ -1,4 +1,5 @@
-from random import randrange
+import random
+from enum import Enum
 
 maxTempIncrement = 5
 minTempIncrement = 1
@@ -8,28 +9,53 @@ offTemp = 100
 onTemp = 95
 
 
+class BulbState(Enum):
+    OFF = 1
+    DIM = 2
+    ON = 3
+
+
 def randomIncrease() -> int:
-    return randrange(minTempIncrement, maxTempIncrement + 1)
+    return random.randrange(minTempIncrement, maxTempIncrement + 1)
 
 
 def randomDecrease() -> int:
-    return randrange(minTempDecrement, maxTempDecrement + 1)
+    return random.randrange(minTempDecrement, maxTempDecrement + 1)
 
 
 class FlickerBulb:
-    def __init__(self):
-        self.isOn = True
+    def __init__(self, seed: int = None):
+        self.state = BulbState.DIM
         self.temp = 0
+        random.seed(seed)
 
     def tick(self):
-        if self.isOn == True:
+        if self.state == BulbState.DIM:
+            if self.temp < onTemp:
+                self.finishTurningOn()
+            else:
+                self.finishTurningOff()
+
+        elif self.state == BulbState.ON:
             self.temp = self.temp + randomIncrease()
 
             if self.temp >= offTemp:
-                self.isOn = False
+                self.turnOff()
 
-        else:
+        elif self.state == BulbState.OFF:
             self.temp = self.temp - randomDecrease()
 
             if self.temp <= onTemp:
-                self.isOn = True
+                self.turnOn()
+
+    def turnOff(self):
+        self.state = BulbState.DIM
+
+    def turnOn(self):
+        self.state = BulbState.DIM
+
+    def finishTurningOff(self):
+        self.state = BulbState.OFF
+
+    def finishTurningOn(self):
+        self.state = BulbState.ON
