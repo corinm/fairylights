71e83ca47b7b93a7091f18db6882c71187fe78dd
Corin Mulliss <corinm@users.noreply.github.com>
2021-05-29 11:06:18 +0100

    Added extra tests, flicker now supports dim colour

--- ledColours.py
+++ ledColours.py
@@ -20,6 +20,7 @@ flicker2_4V = Color(rgb=(69/255, 40/255, 2/255))  # Dimmer
 flicker3V = Color(rgb=(96/255, 145/255, 3/255))  # Bright
 
 flickerColours = list(flicker3V.range_to(flicker2_4V, 11))
+flickerMid = flickerColours[5]  # For "DIM" flicker bulbs
 
 # printColour(flickerColours[4])
 
