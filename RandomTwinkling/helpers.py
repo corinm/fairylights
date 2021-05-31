98770b219de5bb174c88920ae58716e7d6dcfac1
Corin Mulliss <corinm@users.noreply.github.com>
2021-05-31 17:19:23 +0100

    Twinkle shuffle works, added tests

---
+++ RandomTwinkling/helpers.py
@@ -0,0 +1,34 @@
+import math
+import random
+from typing import List
+
+
+def createShuffledList(numberOfBulbs, random):
+    numberOfBuckets = math.floor(numberOfBulbs / 10)
+    buckets = [i for i in range(numberOfBuckets)]
+    remainingIndexes: List[int] = [i for i in range(numberOfBulbs)]
+    remainingIndexesByBucket: List[List[int]] = [[]
+                                                 for i in range(numberOfBuckets)]
+
+    for n in remainingIndexes:
+        bucketIndex = math.floor(n / 10)
+        remainingIndexesByBucket[bucketIndex].append(n)
+
+    shuffledBulbIndexes: List[int] = []
+
+    while len(remainingIndexes) > 0:
+        random.shuffle(buckets)
+
+        for bucketIndex in buckets:
+            remainingIndexesInBucket = remainingIndexesByBucket[bucketIndex]
+            chosenIndex = remainingIndexesInBucket[random.randrange(
+                0, len(remainingIndexesInBucket))]
+
+            remainingIndexesInBucket.remove(chosenIndex)
+            remainingIndexes.remove(chosenIndex)
+
+            shuffledBulbIndexes.append(chosenIndex)
+
+    assert len(shuffledBulbIndexes) == numberOfBulbs
+
+    return shuffledBulbIndexes
