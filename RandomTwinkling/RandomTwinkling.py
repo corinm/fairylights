ec8a26c4b17032ceb892f3ef7cc4717e22d6275a
Corin Mulliss <corin.mulliss@gmail.com>
2021-05-31 17:19:23 +0100

    Twinkle shuffle works, added tests

--- RandomTwinkling/RandomTwinkling.py
+++ RandomTwinkling/RandomTwinkling.py
@@ -1,41 +1,37 @@
-from typing import Dict, List
+from typing import List
+
+import helpers
 
 
 class RandomTwinkling:
     def __init__(self, numberOfBulbs: int):
-        self.numberOfBulbs = numberOfBulbs
+        self.numberOfBulbs: int = numberOfBulbs
+        self.counter: int = 0
         self.shuffledBulbIndexes: List[int] = []
-        self.counter = 0
-
-        # Divide bulbs into buckets of 10
-        self.buckets: Dict[int, bool] = {}
-        numberOfBuckets = int(numberOfBulbs / 10)
-        for i in range(numberOfBuckets):
-            self.buckets[i] = False
+        self.shuffle()
 
     def tick(self):
         if (self.counter == len(self.shuffledBulbIndexes)):
-            self.createShuffledList()
+            self.shuffle()
             self.counter = 0
-        pass
 
         # Return the next bulb and increment counter
         bulb = self.shuffledBulbIndexes[self.counter]
         self.counter += 1
         return bulb
 
-    def createShuffledList(self):
-        # Choose random bucket
-        # [0,1,2,3,4] buckets - shuffle
-        # pick in turn, when get to end, shuffle and repeat
-        # Until all bulbs have been picked
-        # Perhaps need a datra structure to represent the buckets - hashmap?
-        # Otherwise it'll be hard to keep track of what's already been picked
+    def shuffle(self):
+        self.shuffledBulbIndexes = helpers.createShuffledList(
+            self.numberOfBulbs)
 
-        # Pick random bulb from bucket
-        # Append bulb to list
 
-        # From remaining buckets, repeat
-
-        # If all buckets have been selected from, reset buckets
-        pass
+rt = RandomTwinkling(50)
+print(rt.counter, rt.shuffledBulbIndexes)
+rt.tick()
+print(rt.counter, rt.shuffledBulbIndexes)
+rt.tick()
+print(rt.counter, rt.shuffledBulbIndexes)
+rt.tick()
+print(rt.counter, rt.shuffledBulbIndexes)
+rt.tick()
+print(rt.counter, rt.shuffledBulbIndexes)
---
+++ RandomTwinkling/helpers.test.py
@@ -0,0 +1,112 @@
+from random import shuffle
+import unittest
+
+import helpers
+import random
+import functools
+import collections
+
+
+class TestHelpers(unittest.TestCase):
+    def test_generates_a_list_of_50_bulbs(self):
+        shuffled = helpers.createShuffledList(50, random)
+        self.assertEqual(len(shuffled), 50)
+
+    def test_list_is_evenly_shuffled(self):
+        random.seed(1)
+        shuffled = helpers.createShuffledList(50, random)
+
+        # 0-5 contain one number between 0 and 9, one between 10 and 19 etc.
+        divided = map(lambda x: int(x / 10), shuffled[0:5])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+        divided = map(lambda x: int(x / 10), shuffled[5:10])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+        divided = map(lambda x: int(x / 10), shuffled[10:15])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+        divided = map(lambda x: int(x / 10), shuffled[15:20])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+        divided = map(lambda x: int(x / 10), shuffled[20:25])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+        divided = map(lambda x: int(x / 10), shuffled[25:30])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+        divided = map(lambda x: int(x / 10), shuffled[30:35])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+        divided = map(lambda x: int(x / 10), shuffled[35:40])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+        divided = map(lambda x: int(x / 10), shuffled[40:45])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+        divided = map(lambda x: int(x / 10), shuffled[45:50])
+        counter = collections.Counter(divided)
+        self.assertEqual(counter.get(0), 1)
+        self.assertEqual(counter.get(1), 1)
+        self.assertEqual(counter.get(2), 1)
+        self.assertEqual(counter.get(3), 1)
+        self.assertEqual(counter.get(4), 1)
+
+    def test_every_number_appears_once(self):
+        random.seed(1)
+        shuffled = helpers.createShuffledList(50, random)
+
+        # 0-5 contain one number between 0 and 9, one between 10 and 19 etc.
+        counter = collections.Counter(shuffled)
+
+        for i in range(50):
+            self.assertEqual(counter.get(i), 1)
+
+
+if __name__ == '__main__':
+    unittest.main()
