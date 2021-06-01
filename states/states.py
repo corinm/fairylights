53e98bc6164ae0c9bfa87f9d7384a309353d665a
Corin Mulliss <corinm@users.noreply.github.com>
2021-06-01 19:09:09 +0100

    Testing out threads

--- states/states.py
+++ states/states.py
@@ -3,26 +3,29 @@ from transitions import Machine, State
 from flickering_fairylights import FlickeringFairyLights
 from random_twinkling.RandomTwinkling import RandomTwinkling
 
-states = [
-    State(name='Init', on_enter=['initialise']),
-    State(name='FlickeringFairyLights', on_enter=['runFlickeringFairyLights']),
-    State(name='RandomTwinkling', on_enter=['runRandomTwinkling']),
-    State(name='Fireflies', on_enter=['runFireflies'])]
-
-transitions = [
-    ['initialise', states[0], states[1]],
-    {'trigger': 'buttonPressed', 'source': states[1], 'dest': states[2]},
-    {'trigger': 'buttonPressed', 'source': states[2], 'dest': states[3]},
-    {'trigger': 'buttonPressed', 'source': states[3], 'dest': states[1]},
-]
-
 
 class FairyLights(object):
+
+    states = [
+        State(name='Init', on_enter=['initialise']),
+        State(name='FlickeringFairyLights',
+              on_enter=['runFlickeringFairyLights']),
+        State(name='RandomTwinkling', on_enter=['runRandomTwinkling']),
+        State(name='Fireflies', on_enter=['runFireflies'])]
+
+    transitions = [
+        {'trigger': 'initialise', 'source': states[0], 'dest': states[1]},
+        {'trigger': 'buttonPressed', 'source': states[1], 'dest': states[2]},
+        {'trigger': 'buttonPressed', 'source': states[2], 'dest': states[3]},
+        {'trigger': 'buttonPressed', 'source': states[3], 'dest': states[1]},
+    ]
+
     def __init__(self, button, display):
+        print('Starting...')
         self.button = button
         self.display = display
-        self.machine = Machine(self, states=states,
-                               transitions=transitions, initial=states[0], after_state_change='showState')
+        self.machine = Machine(self, states=FairyLights.states,
+                               transitions=FairyLights.transitions, initial=FairyLights.states[0], after_state_change='showState')
         self.to_FlickeringFairyLights()
 
     def showState(self):
@@ -34,7 +37,8 @@ class FairyLights(object):
         ff = FlickeringFairyLights()
 
         while True:
-            ff.tick()
+            # ff.tick()
+            pass
 
     def runRandomTwinkling(self):
         print('Twinkle')
@@ -46,11 +50,12 @@ class FairyLights(object):
     def runFireflies(self):
         print('*** 3')
 
+    def buttonPressed(self):
+        print('Button pressed')
+
 
 def main(button, display):
-    print('Starting...')
     fl = FairyLights(button, display)
-    fl.initialise()
     fl.buttonPressed()
     fl.buttonPressed()
     fl.buttonPressed()
