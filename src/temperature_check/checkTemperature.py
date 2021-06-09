1f2adc3867adc7650419f82359adb10fdc5364c5
Corin Mulliss <corinm@users.noreply.github.com>
2021-06-09 14:38:18 +0100

    Removed unused code, simplified states

--- src/temperature_check/checkTemperature.py
+++
@@ -1,7 +0,0 @@
-from gpiozero import CPUTemperature
-
-cpu = CPUTemperature()
-
-
-def checkTemperature() -> bool:
-    return cpu.temperature > 60
--- src/temperature_check/mockCheckTemperature.py
+++
@@ -1,2 +0,0 @@
-def mockCheckTemperature() -> bool:
-    return False
