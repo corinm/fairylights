af0e6b3d3ea36477dc7c77cd9677daf1cc42c89a
Corin Mulliss <corin.mulliss@gmail.com>
2021-06-16 13:29:13 +0100

    Removed commented out code

--- src/random_twinkling/RandomTwinkling.py
+++ src/random_twinkling/RandomTwinkling.py
@@ -11,28 +11,48 @@ STEPS_FROM_OFF_TO_ON = 19
 NUMBER_OF_STATES = STEPS_FROM_OFF_TO_ON * 2 - 1
 
 
-class RandomTwinkling:
-    def __init__(self, numberOfBulbs: int, colours: List[Color]):
+class ShuffledBulbs:
+    def __init__(self, numberOfBulbs: int):
         self.numberOfBulbs: int = numberOfBulbs
         self.counter: int = 0
-
-        self.updateColours(colours)
-
-        self.currentColourIndex: int = 0
-
         self.shuffledBulbIndexes: List[int] = []
         self._shuffle()
-
         self.bulbs: List[TwinkleBulb] = [
             TwinkleBulb(NUMBER_OF_STATES) for _ in range(numberOfBulbs)
         ]
 
+    def _shuffle(self):
+        self.shuffledBulbIndexes = helpers.createShuffledList(self.numberOfBulbs)
+
+    def getNextBulb(self) -> TwinkleBulb:
+        bulbIndex: int = self.shuffledBulbIndexes[self.counter]
+        bulb: TwinkleBulb = self.bulbs[bulbIndex]
+        self._incrementCounter()
+        return bulb
+
+    def _incrementCounter(self):
+        self.counter += 1
+
+        if self.counter == len(self.shuffledBulbIndexes):
+            self._shuffle()
+            self.counter = 0
+
+    def getBulbs(self) -> List[TwinkleBulb]:
+        return self.bulbs
+
+
+class RandomTwinkling:
+    def __init__(self, numberOfBulbs: int, colours: List[Color]):
+        self.updateColours(colours)
+        self.currentColourIndex: int = 0
+        self.shuffledBulbs = ShuffledBulbs(numberOfBulbs)
+
     def tick(self) -> List[Color]:
         self._nextTwinkle()
 
-        colours: List[Color] = [bulb.getColour() for bulb in self.bulbs]
+        colours: List[Color] = [bulb.getColour() for bulb in self.shuffledBulbs.getBulbs()]
 
-        self._updateBulbStates()
+        [bulb.tick() for bulb in self.shuffledBulbs.getBulbs()]
 
         return colours
 
@@ -43,30 +63,14 @@ class RandomTwinkling:
         self.stateToColourByColour: List[List[Color]] = []
 
         for i in range(self.numberOfColours):
-            stateToColour = createGradientFromAndToBlack(
-                colours[i].hex, STEPS_FROM_OFF_TO_ON
-            )
+            stateToColour = createGradientFromAndToBlack(colours[i].hex, STEPS_FROM_OFF_TO_ON)
             self.stateToColourByColour.append(stateToColour)
 
         self.currentColourIndex = 0
         print(time() - t1)
 
-    def _incrementColour(self):
-        self.currentColourIndex += 1
-
-        if self.currentColourIndex >= self.numberOfColours:
-            self.currentColourIndex = 0
-
-    def _shuffle(self):
-        self.shuffledBulbIndexes = helpers.createShuffledList(self.numberOfBulbs)
-
     def _nextTwinkle(self):
-        if self.counter == len(self.shuffledBulbIndexes):
-            self._shuffle()
-            self.counter = 0
-
-        bulb = self._getNextBulb()
-        self.counter += 1
+        bulb = self.shuffledBulbs.getNextBulb()
 
         # Only start a new twinkle if we're not mid-twinkle from a previous shuffle
         if bulb.isReady():
@@ -74,13 +78,10 @@ class RandomTwinkling:
             bulb.start(colour)
             self._incrementColour()
 
-        return self.bulbs
+        return self.shuffledBulbs.getBulbs()
 
-    def _getNextBulb(self) -> TwinkleBulb:
-        bulbIndex: int = self.shuffledBulbIndexes[self.counter]
-        bulb: TwinkleBulb = self.bulbs[bulbIndex]
-        return bulb
+    def _incrementColour(self):
+        self.currentColourIndex += 1
 
-    def _updateBulbStates(self):
-        for bulb in self.bulbs:
-            bulb.tick()
+        if self.currentColourIndex >= self.numberOfColours:
+            self.currentColourIndex = 0
