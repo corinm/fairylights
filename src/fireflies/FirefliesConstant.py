682d34b8aca0ed6bb3a22f698dfbf601aa86897b
Corin Mulliss <corinm@users.noreply.github.com>
2021-06-19 17:39:07 +0100

    New type of fireflies works

--- src/fireflies/FirefliesConstant.py
+++ src/fireflies/FirefliesConstant.py
@@ -1,4 +1,4 @@
-from random import randrange, shuffle
+from random import random, randrange, shuffle
 from typing import List, Set
 
 from colour import Color
@@ -9,21 +9,21 @@ from .Firefly import Firefly
 from .patterns import staticGlow
 
 
+def shouldAdd() -> bool:
+    return random() < 0.2
+
+
 class FirefliesConstant:
     def __init__(self):
         self.fireflies: List[Firefly] = []
 
     def tick(self) -> List[Color]:
-        takenPositions: Set[int] = set([ff.position for ff in self.fireflies])
-        emptyPositions: List[int] = list(filter(lambda x: x not in takenPositions, range(50)))
-        shuffle(emptyPositions)
-
-        numberOfNewFireflies = randrange(0, 8)
-        for i in range(min(numberOfNewFireflies, len(emptyPositions))):
-            ticksActive = randrange(0, 10)
-            self.fireflies.append(Firefly(emptyPositions[i], staticGlow, ticksActive))
+        if shouldAdd():
+            self._addMoreFireflies()
 
         colours = [off for i in range(50)]
+
+        print(len(self.fireflies))
 
         for firefly in self.fireflies:
             colour = firefly.tick()
@@ -32,3 +32,14 @@ class FirefliesConstant:
         self.fireflies = [f for f in self.fireflies if not f.isDone]
 
         return colours
+
+    def _addMoreFireflies(self):
+        takenPositions: Set[int] = set([ff.position for ff in self.fireflies])
+        emptyPositions: List[int] = list(filter(lambda x: x not in takenPositions, range(50)))
+        shuffle(emptyPositions)
+
+        numberOfNewFireflies = randrange(1, 5)
+        for i in range(min(numberOfNewFireflies, len(emptyPositions))):
+            ticksActive = randrange(0, 5)
+            steps = randrange(4, 9)
+            self.fireflies.append(Firefly(emptyPositions[i], staticGlow, ticksActive, steps=steps))
