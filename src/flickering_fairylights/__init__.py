041301e7bac2ec588ef04c200e5ba411c9b6f0ba
Corin Mulliss <corinm@users.noreply.github.com>
2021-07-03 22:44:35 +0100

    Commented out cycle as it breaks

--- src/flickering_fairylights/__init__.py
+++ src/flickering_fairylights/__init__.py
@@ -1,8 +1,12 @@
+from random import random
+
 from .FlickeringFairyLights import FlickeringFairyLights
 
 
 def run(leds):
     ff = FlickeringFairyLights()
+    id = random()
 
     while True:
+        print("Loop", id)
         leds.setLeds(ff.tick())
