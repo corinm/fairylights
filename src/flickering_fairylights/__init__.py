b1587811dbca3d4a5b3515623bba3fe9721c3319
Corin Mulliss <corin.mulliss@gmail.com>
2021-07-07 13:31:25 +0100

    Tidying old logs

--- src/flickering_fairylights/__init__.py
+++ src/flickering_fairylights/__init__.py
@@ -1,12 +1,8 @@
-from random import random
-
 from .FlickeringFairyLights import FlickeringFairyLights
 
 
 def run(leds):
     ff = FlickeringFairyLights()
-    id = random()
 
     while True:
-        print("Loop", id)
         leds.setLeds(ff.tick())
