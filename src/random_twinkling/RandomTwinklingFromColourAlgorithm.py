1dca81d695dcac1d02b6178ede528ef19563d242
Corin Mulliss <corin.mulliss@gmail.com>
2021-07-21 16:42:51 +0100

    Clearer logging

--- src/random_twinkling/RandomTwinklingFromColourAlgorithm.py
+++ src/random_twinkling/RandomTwinklingFromColourAlgorithm.py
@@ -38,7 +38,7 @@ class RandomTwinklingFromColourAlgorithm:
         self.colours: List[Color] = [
             self.randomColourAlgorithm() for _ in range(self.numberOfColours)
         ]
-        print("New palette:", [c.hex for c in self.colours])
+        print("🌈  New palette:", [c.hex for c in self.colours])
         self.rt = RandomTwinkling(self.numberOfLeds, self.colours)
 
         self._resetNewPaletteTime()
@@ -58,7 +58,7 @@ class RandomTwinklingFromColourAlgorithm:
         self.colours: List[Color] = [
             self.randomColourAlgorithm() for _ in range(self.numberOfColours)
         ]
-        print("New palette:", [c.hex for c in self.colours])
+        print("🌈  New palette:", [c.hex for c in self.colours])
         self.rt.updateColours(self.colours)
         self._resetNewPaletteTime()
 
