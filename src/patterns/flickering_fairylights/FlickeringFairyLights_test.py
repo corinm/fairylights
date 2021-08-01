02190f0cb3755952cf8ba884fd8b5d0a7da76396
Corin Mulliss <corin.mulliss@gmail.com>
2021-08-01 16:54:41 +0100

    Fixed tests

--- src/patterns/flickering_fairylights/FlickeringFairyLights_test.py
+++ src/patterns/flickering_fairylights/FlickeringFairyLights_test.py
@@ -1,8 +1,8 @@
 import unittest
 
-import patterns.flickering_fairylights.constants as constants
 import patterns.flickering_fairylights.helpers as helpers
 from patterns.flickering_fairylights.FlickerBulb import BulbState
+from utils.colours import _flicker2_4V, _flicker3V, flickerColours, off
 
 
 class TestFlickeringFairyLight(unittest.TestCase):
@@ -11,7 +11,7 @@ class TestFlickeringFairyLight(unittest.TestCase):
         self.assertEqual(len(colours), 50)
 
         for i in range(50):
-            self.assertEqual(colours[i], constants.flicker2_4V)
+            self.assertEqual(colours[i], _flicker2_4V)
 
     def test_all_flicker_bulbs_off_returns_all_3V_except_flicker_bulbs(self):
         colours = helpers.generateFullListOfColours([BulbState.OFF for i in range(10)])
@@ -19,9 +19,9 @@ class TestFlickeringFairyLight(unittest.TestCase):
 
         for i in range(50):
             if i % 5 == 0:
-                self.assertEqual(colours[i], constants.off)
+                self.assertEqual(colours[i], off)
             else:
-                self.assertEqual(colours[i], constants.flicker3V)
+                self.assertEqual(colours[i], _flicker3V)
 
     def test_bulbs_are_correct_colours_1ON_1DIM(self):
         colours = helpers.generateFullListOfColours(
@@ -42,13 +42,13 @@ class TestFlickeringFairyLight(unittest.TestCase):
 
         for i in range(50):
             if i == 0:
-                self.assertEqual(colours[i], constants.flickerColours[1])
+                self.assertEqual(colours[i], flickerColours[1])
             elif i == 5:
-                self.assertEqual(colours[i], constants.flickerMid)
+                self.assertEqual(colours[i], flickerColours[5])
             elif i % 5 == 0:
-                self.assertEqual(colours[i], constants.off)
+                self.assertEqual(colours[i], off)
             else:
-                self.assertEqual(colours[i], constants.flickerColours[1])
+                self.assertEqual(colours[i], flickerColours[1])
 
     def test_bulbs_are_correct_colours_1ON_2DIM(self):
         colours = helpers.generateFullListOfColours(
@@ -69,13 +69,13 @@ class TestFlickeringFairyLight(unittest.TestCase):
 
         for i in range(50):
             if i == 0:
-                self.assertEqual(colours[i], constants.flickerColours[2])
+                self.assertEqual(colours[i], flickerColours[2])
             elif i == 5 or i == 10:
-                self.assertEqual(colours[i], constants.flickerMid)
+                self.assertEqual(colours[i], flickerColours[5])
             elif i % 5 == 0:
-                self.assertEqual(colours[i], constants.off)
+                self.assertEqual(colours[i], off)
             else:
-                self.assertEqual(colours[i], constants.flickerColours[2])
+                self.assertEqual(colours[i], flickerColours[2])
 
 
 if __name__ == "__main__":
