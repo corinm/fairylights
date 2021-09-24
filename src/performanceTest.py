837b4c49eaf57d14fe394e781163b765d31911f4
Corin Mulliss <corinm@users.noreply.github.com>
2021-09-24 16:04:01 +0100

    Slightly different performance test

--- src/performanceTest.py
+++ src/performanceTest.py
@@ -2,14 +2,21 @@ from line_profiler import LineProfiler
 from colour import Color
 
 from patterns.twinkle.Twinkle import Twinkle
+from patterns.twinkle.TwinkleBulb import TwinkleBulb
 
 lp = LineProfiler()
 
 def main():
-    t = Twinkle(150, [Color('red'), Color('blue', Color('green'))])
-    t.tick()
+    t = Twinkle(1000, [Color('red'), Color('blue', Color('green'))])
     lp_wrapper = lp(t.tick)
     lp_wrapper()
     lp.print_stats()
+
+# def main():
+#     t = TwinkleBulb(0.8, 0.2)
+#     t.setColourAtPeak(Color('red'))
+#     lp_wrapper = lp(t.incrementTimeDelta)
+#     lp_wrapper(0.004)
+#     lp.print_stats()
 
 main()
