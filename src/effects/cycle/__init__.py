726d2163802de618208c55288ef42b43a1fb9605
Corin Mulliss <corinm@users.noreply.github.com>
2021-11-27 21:30:02 +0000

    Faster rainbow cycle

--- src/effects/cycle/__init__.py
+++ src/effects/cycle/__init__.py
@@ -7,18 +7,16 @@ from ..Effect import Effect
 from colours.Colours import Colours
 
 
-SECONDS_TO_CYCLE_ONCE = 10
-
-
 class Cycle(Effect):
     def __init__(self, numberOfBulbs: int, colours: Colours):
         Effect.__init__(self)
 
-        self._colours: Colours = colours
+        self._colours = colours.getColours(numberOfBulbs)
         self._numberOfBulbs = numberOfBulbs
-        self._currentColours = self._colours.getColours(self._numberOfBulbs)
-        self._timeBetweenUpdates = 0.08
+        self._currentColours = self._colours
+        self._timeBetweenUpdates = 0.001
         self._timeOfNextUpdate = self._time + self._timeBetweenUpdates
+        self._index = 0
 
     def tick(self) -> List[Color]:
         now = time()
@@ -31,10 +29,13 @@ class Cycle(Effect):
         if now < self._timeOfNextUpdate:
             return self._currentColours
 
-        self._currentColours = self._currentColours[1:] + self._currentColours[0:1]
+        colours = self._colours[self._index:] + self._colours[0:self._index]
+        self._index += 3
+        if self._index >= self._numberOfBulbs:
+            self._index -= self._numberOfBulbs
 
         super().tick(now, self._currentColours)
-        
+
         self._timeOfNextUpdate = now + self._timeBetweenUpdates
 
-        return self._currentColours
+        return colours
